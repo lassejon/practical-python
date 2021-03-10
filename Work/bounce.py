a8333165d17692f4a1e85438eea621e8ae34f2ce
Lasse Jon Marquardt Frederiksen <lassejon@Lasses-MacBook-Pro.local>
2021-03-10 10:26:30 +0100

    just saving my progress

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+i = 0
+
+while i <= 9:
+    i +=1
+    height = round(height * 3/5, 4)
+    print(height)
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,31 @@
 # mortgage.py
 #
 # Exercise 1.7
+mortgage = 500000
+rate = .05
+payment = 2684.11
+total_paid = 0
+month = 1
+extra_payment_start_month = 12*5 + month
+extra_payment_end_month = 12*9
+extra_payment = 1000
+
+while mortgage > 0:
+    mortgage = mortgage * (1 + rate/12) - payment
+    total_paid += payment
+
+    if (month >= extra_payment_start_month and 
+        month <= extra_payment_end_month):
+        mortgage -= extra_payment
+        total_paid += extra_payment
+
+    if mortgage < 0:
+       total_paid += mortgage
+       mortgage = 0
+
+    print(month, total_paid, mortgage)
+
+    month += 1
+
+print(mortgage, round(total_paid, 2))
+print("Months:", month - 1)
